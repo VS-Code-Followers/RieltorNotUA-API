14dafb856378e047d7b340d697163b543fa35fdd
CBoYXD <python.rust.cpp@gmail.com>
2024-02-19 19:12:19 +0200

    add new funcs in config.py

--- src/config.py
+++ src/config.py
@@ -2,6 +2,8 @@ from functools import lru_cache
 from os import getenv
 from pydantic import BaseModel
 from pydantic_settings import BaseSettings, SettingsConfigDict
+from sqlalchemy import URL
+from sqlalchemy.ext.asyncio import create_async_engine, AsyncEngine, async_sessionmaker
 
 
 class FastAPI(BaseModel):
@@ -29,3 +31,20 @@ class Config(BaseSettings):
 @lru_cache
 def get_config() -> Config:
     return Config()
+
+
+def get_engine(config: DataBase) -> AsyncEngine:
+    return create_async_engine(
+        URL.create(
+            config.driver,
+            config.user,
+            config.password,
+            config.host,
+            config.port,
+            config.name,
+        )
+    )
+
+
+def get_session_pool():
+    return async_sessionmaker(bind=get_engine(get_config()))
