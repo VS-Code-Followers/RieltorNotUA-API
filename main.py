ef4d3b9580eae968929edbd6800039946d837ef6
CBoYXD <python.rust.cpp@gmail.com>
2024-03-01 22:41:06 +0200

    Add new exception handlers

--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI, Request
 from uvicorn import run
 from fastapi.responses import RedirectResponse, JSONResponse
+from fastapi.exceptions import RequestValidationError
 from sqlalchemy.exc import SQLAlchemyError
+from pydantic import ValidationError
 import starlette.status as status
 from src.api.routers import search, account, query
 from src.config import get_config
@@ -19,6 +21,32 @@ async def root():
 
 @app.exception_handler(SQLAlchemyError)
 async def hand_db_exceptions(request: Request, exc: SQLAlchemyError):
+    return JSONResponse(
+        status_code=400,
+        content={'detail': str(exc)},
+    )
+
+
+@app.exception_handler(ValidationError)
+async def hand_validation_exceptions(request: Request, exc: ValidationError):
+    return JSONResponse(
+        status_code=400,
+        content={'detail': exc.errors()},
+    )
+
+
+@app.exception_handler(RequestValidationError)
+async def hand_request_validation_exceptions(
+    request: Request, exc: RequestValidationError
+):
+    return JSONResponse(
+        status_code=400,
+        content={'detail': exc.errors()},
+    )
+
+
+@app.exception_handler(Exception)
+async def hand_exceptions(request: Request, exc: AttributeError):
     return JSONResponse(
         status_code=400,
         content={'detail': str(exc)},
