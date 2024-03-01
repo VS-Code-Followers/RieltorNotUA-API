96e7edcd46f2d73133e1cb4aa664fe5f3022e99d
CBoYXD <python.rust.cpp@gmail.com>
2024-03-01 19:14:00 +0200

    Add DataBase Exceptions

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Request
 from uvicorn import run
-from fastapi.responses import RedirectResponse
+from fastapi.responses import RedirectResponse, JSONResponse
+from sqlalchemy.exc import SQLAlchemyError
 import starlette.status as status
 from src.api.routers import search, account, query
 from src.config import get_config
@@ -14,6 +15,14 @@ for router in [search.router, account.router, query.router]:
 @app.get('/')
 async def root():
     return RedirectResponse(url='/docs', status_code=status.HTTP_302_FOUND)
+
+
+@app.exception_handler(SQLAlchemyError)
+async def hand_db_exceptions(request: Request, exc: SQLAlchemyError):
+    return JSONResponse(
+        status_code=400,
+        content={'detail': str(exc)},
+    )
 
 
 if __name__ == '__main__':
