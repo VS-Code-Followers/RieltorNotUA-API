f2643fab8cd80f6762bf53bf226286dba2a6be58
YuriiVid <yuriividoniak@gmail.com>
2024-04-02 20:36:57 +0300

    add OAuth2 scopes

--- src/api/auth/__init__.py
+++ src/api/auth/__init__.py
@@ -1,10 +1,15 @@
-from fastapi.security import OAuth2PasswordRequestForm, OAuth2PasswordBearer, OAuth2PasswordRequestFormStrict
-from fastapi import logger,HTTPException
+from fastapi.security import (
+    OAuth2PasswordRequestForm,
+    OAuth2PasswordBearer,
+    OAuth2PasswordRequestFormStrict,
+    SecurityScopes,
+)
+from fastapi import logger, HTTPException
 from ..models.users import AuthorInDB
 from jose import JWTError, jwt
 from passlib.context import CryptContext
 from datetime import datetime, timedelta, timezone
-from pydantic import BaseModel, EmailStr
+from pydantic import BaseModel, EmailStr, ValidationError
 from .models import Token, TokenData
 from fastapi import Depends, status
 from typing import Optional, Annotated
@@ -14,18 +19,20 @@ SECRET_KEY = "09d25e094faa6ca2556c818166b7a9563b93f7099f6f0f4caa6cf63b88e8d3e7"
 ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 30
 
-oauth2_scheme = OAuth2PasswordBearer(tokenUrl="account/token")
+oauth2_scheme = OAuth2PasswordBearer(
+    tokenUrl="account/token",
+    scopes={"me": "Read information about the current user.", "items": "Read items."},
+)
 
 fake_users_db = {
     "johndoe@example.com": {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "password": '$2b$12$DX/CgXx3.vwKSENi0mUZHeK.l94QqIPs9kWjaSu2CdfEmu2TfdrD2',
+        "password": "$2b$12$DX/CgXx3.vwKSENi0mUZHeK.l94QqIPs9kWjaSu2CdfEmu2TfdrD2",
         "disabled": False,
     }
 }
-
 
 
 pwd_context = CryptContext(schemes=["bcrypt"], deprecated="auto")
@@ -42,7 +49,6 @@ def get_password_hash(password):
 def get_user(db, email) -> AuthorInDB:
     if email in db:
         return AuthorInDB(**db[email])
-    
 
 
 def authenticate_user(fake_db, email: str, password: str) -> AuthorInDB:
@@ -52,6 +58,7 @@ def authenticate_user(fake_db, email: str, password: str) -> AuthorInDB:
     if not verify_password(password, user.password):
         return False
     return user
+
 
 def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     to_encode = data.copy()
@@ -64,22 +71,40 @@ def create_access_token(data: dict, expires_delta: Optional[timedelta] = None):
     return encoded_jwt
 
 
-def get_current_user(token: Annotated[str, Depends(oauth2_scheme)]):
+def get_current_user(
+    security_scopes: SecurityScopes, token: Annotated[str, Depends(oauth2_scheme)]
+):
+    if security_scopes.scopes:
+        authenticate_value = f'Bearer scope="{security_scopes.scope_str}"'
+    else:
+        authenticate_value = "Bearer"
+
     credentials_exception = HTTPException(
         status_code=status.HTTP_401_UNAUTHORIZED,
         detail="Could not validate credentials",
-        headers={"WWW-Authenticate": "Bearer"},
+        headers={"WWW-Authenticate": authenticate_value},
     )
+
     try:
         payload = jwt.decode(token, SECRET_KEY, algorithms=[ALGORITHM])
         email: EmailStr = payload.get("sub")
         if email is None:
             raise credentials_exception
-        token_data = TokenData(email=email)
-    except JWTError:
+        token_scopes = payload.get("scopes", [])
+        token_data = TokenData(scopes=token_scopes, email=email)
+    except (JWTError, ValidationError):
         raise credentials_exception
+
     user = get_user(fake_users_db, email=token_data.email)
     if user is None:
         raise credentials_exception
-    return user
 
+    for scope in security_scopes.scopes:
+        if scope not in token_data.scopes:
+            raise HTTPException(
+                status_code=status.HTTP_401_UNAUTHORIZED,
+                detail="Not enough permissions",
+                headers={"WWW-Authenticate": authenticate_value},
+            )
+
+    return user
--- src/api/routers/account.py
+++ src/api/routers/account.py
@@ -1,14 +1,21 @@
-from fastapi import logger,  APIRouter, Depends, HTTPException, status
+from fastapi import Security, logger, APIRouter, Depends, HTTPException, status
 from fastapi.security import OAuth2PasswordRequestForm
-from ..auth import oauth2_scheme, Token, authenticate_user, create_access_token, get_current_user, ACCESS_TOKEN_EXPIRE_MINUTES
+from ..auth import (
+    oauth2_scheme,
+    Token,
+    authenticate_user,
+    create_access_token,
+    get_current_user,
+    ACCESS_TOKEN_EXPIRE_MINUTES,
+)
 from ..auth.models import OAuth2Form
 from ..models.users import Author
 from typing import Annotated
 from datetime import timedelta
 
 router = APIRouter(
-    prefix='/account',
-    tags=['account'],
+    prefix="/account",
+    tags=["account"],
 )
 
 """
@@ -35,35 +42,35 @@ fake_users_db = {
         "username": "johndoe",
         "full_name": "John Doe",
         "email": "johndoe@example.com",
-        "password": '$2b$12$DX/CgXx3.vwKSENi0mUZHeK.l94QqIPs9kWjaSu2CdfEmu2TfdrD2',
+        "password": "$2b$12$DX/CgXx3.vwKSENi0mUZHeK.l94QqIPs9kWjaSu2CdfEmu2TfdrD2",
         "disabled": False,
     }
 }
 
-@router.get('/')
+
+@router.get("/")
 async def root_account() -> dict[str, str]:
     return {
-        'account': 'there will be information about possible settings for user (maybe 😁)'
+        "account": "there will be information about possible settings for user (maybe 😁)"
     }
-    
-    
-    
+
+
 @router.post("/token")
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
-    logger.logger.info(form_data)
     user = authenticate_user(fake_users_db, form_data.username, form_data.password)
-    logger.logger.info(user)
     if not user:
         raise HTTPException(
             status_code=status.HTTP_401_UNAUTHORIZED,
             detail="Incorrect username or password",
             headers={"WWW-Authenticate": "Bearer"},
         )
+
     access_token_expires = timedelta(minutes=ACCESS_TOKEN_EXPIRE_MINUTES)
     access_token = create_access_token(
-        data={"sub": user.email}, expires_delta=access_token_expires
+        data={"sub": user.email, "scopes": form_data.scopes},
+        expires_delta=access_token_expires,
     )
     return Token(access_token=access_token, token_type="bearer")
 
@@ -77,6 +84,6 @@ async def read_users_me(
 
 @router.get("/users/me/items/")
 async def read_own_items(
-    current_user: Annotated[Author, Depends(get_current_user)],
+    current_user: Annotated[Author, Security(get_current_user, scopes=["items"])],
 ):
     return [{"item_id": "Foo", "owner": current_user.email}]
