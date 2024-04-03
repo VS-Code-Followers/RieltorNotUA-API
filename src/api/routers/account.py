ec1a23ec599e3a6aae4974955d23ad2fe41bbc6b
CBoYXD <python.rust.cpp@gmail.com>
2024-04-03 18:24:56 +0300

    Change requests to httpx

--- src/api/routers/account.py
+++ src/api/routers/account.py
@@ -1,5 +1,5 @@
 from fastapi import logger, APIRouter, Depends, HTTPException, status
-import requests
+from httpx import AsyncClient
 from fastapi.security import OAuth2PasswordRequestForm
 from ..auth import (
     oauth2_scheme,
@@ -20,41 +20,44 @@ from src.config import get_config
 config = get_config()
 auth = config.fastapi.auth
 router = APIRouter(
-    prefix="/account",
-    tags=["account"],
+    prefix='/account',
+    tags=['account'],
 )
 
 
-@router.get("/")
+@router.get('/')
 async def root_account() -> dict[str, str]:
     return {
         'account': 'there will be information about possible settings for user (maybe 😁)'
     }
 
-@router.get("/login/google")
+
+@router.get('/login/google')
 async def login_google():
     return {
-        "url": f"https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={auth.google_client_id}&redirect_uri={auth.google_redirect_uri}&scope=openid%20profile%20email&access_type=offline"
+        'url': f'https://accounts.google.com/o/oauth2/auth?response_type=code&client_id={auth.google_client_id}&redirect_uri={auth.google_redirect_uri}&scope=openid%20profile%20email&access_type=offline'
     }
 
 
-@router.get("/auth/google")
+@router.get('/auth/google')
 async def auth_google(code: str):
-    token_url = "https://accounts.google.com/o/oauth2/token"
+    token_url = 'https://accounts.google.com/o/oauth2/token'
     data = {
-        "code": code,
-        "client_id": auth.google_client_id,
-        "client_secret": auth.google_client_secret,
-        "redirect_uri": auth.google_redirect_uri,
-        "grant_type": "authorization_code",
+        'code': code,
+        'client_id': auth.google_client_id,
+        'client_secret': auth.google_client_secret,
+        'redirect_uri': auth.google_redirect_uri,
+        'grant_type': 'authorization_code',
     }
-    response = requests.post(token_url, data=data)
-    access_token = response.json().get("access_token")
-    user_info = requests.get(
-        "https://www.googleapis.com/oauth2/v1/userinfo",
-        headers={"Authorization": f"Bearer {access_token}"},
-    )
-    return user_info.json()
+    async with AsyncClient() as client:
+        response = await client.post(token_url, data=data)
+        access_token = response.json().get('access_token')
+
+        user_info = await client.get(
+            'https://www.googleapis.com/oauth2/v1/userinfo',
+            headers={'Authorization': f'Bearer {access_token}'},
+        )
+        return user_info.json()
 
 
 # @router.get("/token")
@@ -62,7 +65,7 @@ async def auth_google(code: str):
 #     return jwt.decode(token, auth.GOOGLE_CLIENT_SECRET, algorithms=["HS256"])
 
 
-@router.post("/token")
+@router.post('/token')
 async def login_for_access_token(
     form_data: Annotated[OAuth2PasswordRequestForm, Depends()],
 ) -> Token:
