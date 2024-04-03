7d1c411f0ba840ee7edb73a0c160eef690481704
CBoYXD <python.rust.cpp@gmail.com>
2024-04-03 16:19:19 +0300

    Add Users repo

--- src/db/repo/users.py
+++ src/db/repo/users.py
@@ -1,6 +1,35 @@
 from sqlalchemy.ext.asyncio import AsyncSession, AsyncConnection
+from ...api.models.users import AuthorInDB, Author
+from ..models import Users
+from sqlalchemy.ext.asyncio import AsyncSession, AsyncConnection
+from sqlalchemy import insert, select, delete, cast, BIGINT
+from pydantic import EmailStr
+
 
 
 class UserRepo:
     def __init__(self, session: AsyncSession | AsyncConnection):
         self.session = session
+        self.user_params = [
+            "email", "full_name", "account_id", "location", "offers"
+        ]
+
+    async def create_user(self, data: AuthorInDB) -> None:
+        await self.session.execute(insert(Users).values(**data.model_dump()))
+        await self.session.commit()
+
+
+    async def get_all_users(self) -> list[Author]:
+        # !
+        result = await self.session.execute(
+            select(*[cl for cl in Users.__table__.columns if cl.key != "password"])
+        )
+        return [
+            Users(**dict(zip(self.user_params, i._tuple()))) for i in result.fetchall()
+        ]
+        
+    async def get_password_by_email(self, email: EmailStr) -> str:
+        result = await self.session.execute(
+            select(Users.password).where(Users.email == email)
+        )
+        return result.scalars().first()
